b98d90cd5f09d72242082f7869a0f19f5f030171
Luis Siccardi <50088170+LuisSiccardi@users.noreply.github.com>
2022-11-15 11:15:54 -0300

    Update informe.py

--- informe.py
+++ informe.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
-rios_df = pd.read_csv('C:\\Users\\Dell\\Document\\pepe.csv') # Ruta y nombre de archivo.csv
+rios_df = pd.read_csv('C:\\Users\\Dell\\Document\\pepepe.csv') # Ruta y nombre de archivo.csv
 
-# print(rios_df.dtypes) # tipos de datos
-
-print(rios_df.columns) # columnas
+#print(rios_df.columns) # columnas
 
 print(rios_df.dtypes) # tipo de datos
 
-print(pd.unique(rios_df['Puerto']))
+rios_df=rios_df.astype({'Variación': 'float64'})
+
+print(rios_df.dtypes)
+
+mean_df = rios_df['Variación'].mean()
+print(mean_df)
+
+
